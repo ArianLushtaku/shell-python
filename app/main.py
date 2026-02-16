4864ceb12be441b4b93bfbab6f5b3fbde5b0aace
Arian Lushtaku <arianlushtaku@MacBook-Pro-tilhrende-Arian.local>
2026-02-17 00:07:41 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,25 +1,40 @@
 import sys
+import os
+import shutil
 
 commands = {
     "echo": lambda *x: print(" ".join(x)),
     "exit": lambda x=None: exit(),
-    "type": lambda x: print(f"{x} is a shell builtin") if x in commands else print(f'{x}: not found')
+    "type": lambda *x: print(f"{"".join(x[0])} is a shell builtin") if x[0] in commands else pathType(x)
 }
 
+def pathType(x):
+    if path := shutil.which(x[0]):
+        print(f"{x[0]} is {path}")
+    else:
+        print(f'{x}: not found')
+
 def main():
-    # TODO: Uncomment the code below to pass the first stage
+    PATH = "/usr/bin:/usr/local/bin:"
     while True:
-        sys.stdout.write("$ ")
-        pass
+        try:
+            sys.stdout.write("$ ")
+            pass
 
-        user_input = input()
-        parts =  user_input.split()
-        command = parts[0]
-        args = parts[1:]
-        if command in commands:
-            commands[command](*args)
-        else:
-            print(f"{command}: command not found")
+            user_input = input()
+            parts =  user_input.split()
+            command = parts[0]
+            args = parts[1:]
+            if command in commands:
+                commands[command](*args)
+            else:
+                print(f"{command}: command not found")
 
+        except KeyboardInterrupt as e:
+            print("\nExiting!")
+            break
+
+
+    
 if __name__ == "__main__":
     main()

f8829b40ff51554af9c337cff8c3f16014dbe895
Arian Lushtaku <arianlushtaku@MacBook-Pro-tilhrende-Arian.local>
2026-02-17 01:13:15 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,32 +3,34 @@ import os
 import shutil
 import subprocess
 
+
+#Directory of commands as keys and executeables as values.
 commands = {
     "echo": lambda *x: print(" ".join(x)),
     "exit": lambda x=None: exit(),
     "type": lambda *x: print(f"{x[0]} is a shell builtin") if x[0] in commands else pathType(x),
     "pwd": lambda x=None: print(f'{os.getcwd()}') if not x else print(f"pwd: too many arguments"),
-    "cd": lambda x: changeDirectory(x) if x == ".." or os.access(x, os.F_OK) else print(f"cd: no such file or directory: {x}")
+    "cd": lambda x=None: changeDirectory(x) if x is not None and os.access(x, os.F_OK) else None if x is None else print(f"cd: no such file or directory: {x}")
 }
 
+#Type of command, when its not made by me, but is a system executeable command.
 def pathType(x):
     x = x[0]
     if path := shutil.which(x):
         print(f"{x} is {path}")
     else:
         print(f'{x}: not found')
-    
+
+#Change directory command or cd.
 def changeDirectory(x): 
     path = os.getcwd()
     naked_path = path.split(os.path.sep)
-    if x == "..":
-        path = "/".join(naked_path[:-1])
-        os.chdir(path)
-    elif x in os.listdir(path):
+    if x in os.listdir(path):
         os.chdir("/".join(naked_path + x.split(os.path.sep)))
     else:
         os.chdir(x)
 
+#Main code
 def main():
     while True:
         try:
@@ -39,16 +41,19 @@ def main():
             parts =  user_input.split()
             command = parts[0]
             args = parts[1:]
+            #execute commands we defined
             if command in commands:
                 args = args or ()
                 commands[command](*args)
+            #else if command is a system executeable, execute.
             elif (path := shutil.which(command)) and os.access(path, os.X_OK):
                 subprocess.call(parts)
             else:
                 print(f"{command}: command not found")
 
+        #When pressing CTRL C, dont throw error, just close silently with Exiting..
         except KeyboardInterrupt as e:
-            print("\nExiting!")
+            print("\nExiting...")
             break
 
 

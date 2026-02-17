1c1be4b5e51b31b877b1abdb6db3ecc905c7107e
Arian Lushtaku <arianlushtaku@MacBook-Pro-tilhrende-Arian.local>
2026-02-17 01:02:46 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ import subprocess
 commands = {
     "echo": lambda *x: print(" ".join(x)),
     "exit": lambda x=None: exit(),
-    "type": lambda *x: print(f"{"".join(x[0])} is a shell builtin") if x[0] in commands else pathType(x),
-    "pwd": lambda x=None: print(f'{os.getcwd()}')
+    "type": lambda *x: print(f"{x[0]} is a shell builtin") if x[0] in commands else pathType(x),
+    "pwd": lambda x=None: print(f'{os.getcwd()}') if not x else print(f"pwd: too many arguments"),
+    "cd": lambda x: changeDirectory(x) if x == ".." or os.access(x, os.F_OK) else print(f"cd: no such file or directory: {x}")
 }
 
 def pathType(x):
@@ -16,6 +17,17 @@ def pathType(x):
         print(f"{x} is {path}")
     else:
         print(f'{x}: not found')
+    
+def changeDirectory(x): 
+    path = os.getcwd()
+    naked_path = path.split(os.path.sep)
+    if x == "..":
+        path = "/".join(naked_path[:-1])
+        os.chdir(path)
+    elif x in os.listdir(path):
+        os.chdir("/".join(naked_path + x.split(os.path.sep)))
+    else:
+        os.chdir(x)
 
 def main():
     while True:
@@ -28,6 +40,7 @@ def main():
             command = parts[0]
             args = parts[1:]
             if command in commands:
+                args = args or ()
                 commands[command](*args)
             elif (path := shutil.which(command)) and os.access(path, os.X_OK):
                 subprocess.call(parts)
